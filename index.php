ddeb6dff85a3c35db8e91458a3720c6f93513c57
nicollaslopes <nicollaslopesn@gmail.com>
2023-03-13 20:51:35 -0300

    criando o formulário

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Form App</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container d-flex justify-content-center">
+        <form>
+            <div class="mt-4">
+                <h3>Formulário</h3>
+                <p>Por favor, preencha os campos abaixo para enviar seu currículo:</p>
+            </div>
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" id="nome" required>
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" id="email" placeholder="name@example.com" required>
+            </div>
+            <div class="form-group">
+                <label for="telefone">Telefone</label>
+                <input type="text" class="form-control" id="telefone" placeholder="(00) 00000-0000" required>
+            </div>
+            <div class="form-group">
+                <label for="cargo">Cargo desejado</label>
+                <textarea class="form-control" id="cargo" rows="2" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="escolaridade">Escolaridade</label>
+                <select class="form-control" id="escolaridade" required>
+                    <option value="">Selecione uma opção</option>
+                    <option value="fundamental">Ensino Fundamental completo</option>
+                    <option value="medio">Ensino Médio completo</option>
+                    <option value="superior">Ensino Superior completo</option>
+                    <option value="pos">Pós-graduação completo</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="observacoes">Observações</label>
+                <textarea class="form-control" id="observacoes" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="arquivo">Selecione o arquivo ( <i> Extensões permitidas: .doc, .docx ou .pdf </i> )</label>
+                <input type="file" class="form-control" id="arquivo" required>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
